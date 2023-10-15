1042963a4fd730f50113c1781d42d41d7aaab316
poudyalankit <poudyalankit@gmail.com>
2023-10-15 12:00:57 -0400

    added set user function and updated chat room page

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,8 @@
 import sqlite3
 import uuid
 
-from flask import Flask, jsonify
-from flask_cors import CORS, cross_origin
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 conn = sqlite3.connect('chat_rooms.db', check_same_thread=False)
 cursor = conn.cursor()
@@ -30,6 +30,34 @@ def create_chat_room():
     conn.commit()
 
     return jsonify({'chatRoomId': chat_room_id}), 201
+
+
+@app.route('/api/setUser', methods=['POST'])
+def set_user():
+    data = request.get_json()
+    chat_room_id = data.get('route').split("/chat/")[1]
+    cursor.execute("SELECT user1_id, user2_id FROM chat_rooms WHERE id = ?", (chat_room_id,))
+    result = cursor.fetchone()
+
+    if result is not None:
+        user1_id, user2_id = result
+
+        if user1_id is None:
+            user1_id = str(uuid.uuid4())
+            updated_user_id = user1_id
+        elif user2_id is None:
+            user2_id = str(uuid.uuid4())
+            updated_user_id = user2_id
+        else:
+            return jsonify({'message': 'Both user1 and user2 are already set for the chat room'}), 429
+
+        cursor.execute("UPDATE chat_rooms SET user1_id = ?, user2_id = ? WHERE id = ?",
+                       (user1_id, user2_id, chat_room_id))
+        conn.commit()
+
+        return jsonify({'chatRoomId': chat_room_id, 'updatedUserId': updated_user_id}), 201
+    else:
+        return jsonify({'message': 'Chat room not found'}), 404
 
 
 if __name__ == '__main__':
--- frontend/app/chat/[chatRoomId]/page.tsx
+++ frontend/app/chat/[chatRoomId]/page.tsx
@@ -1,11 +1,93 @@
+"use client";
+
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 const ChatRoomPage = () => {
+    const router = useRouter();
+    const [selectedLanguage, setSelectedLanguage] = useState(''); // State to track selected language
 
-  return (
-      <div>
-        <h1>Chat Room</h1>
-      </div>
-  );
+    const setUserAndSetCookie = async () => {
+        const currentRoute = window.location.pathname;
+        const requestData = { route: currentRoute };
+        try {
+            const response = await fetch('http://localhost:3001/api/setUser', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(requestData),
+            });
+
+            if (response.ok) {
+                const { updatedUserId } = await response.json();
+                document.cookie = `${currentRoute}=${updatedUserId}; path=/`;
+            } else {
+                console.error('Failed to set user and cookie');
+                router.push('/');
+            }
+        } catch (error) {
+            console.error('Error setting user and cookie:', error);
+        }
+    };
+
+    useEffect(() => {
+        setUserAndSetCookie().then(r => console.log());
+    }, []);
+
+    const handleLanguageChange = (e: { target: { value: any; }; }) => {
+        const selectedValue = e.target.value;
+        setSelectedLanguage(selectedValue);
+    };
+
+    function handleSendMessage() {
+
+    }
+
+    let message;
+    return (
+        <div className="min-h-screen flex flex-col items-center justify-center">
+            <h1 className="text-2xl font-bold mb-4">Chat Room</h1>
+
+            <div className="chat-box bg-gray-100 p-4 w-1/2 h-1/2 mb-4">
+                    <div className="mb-2">Test message</div>
+            </div>
+
+            <div className="w-1/2">
+                <div className="flex">
+                    <input
+                        type="text"
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                        className="w-full p-2 border border-gray-300 rounded-l-md"
+                        placeholder="Type your message..."
+                    />
+                    <button
+                        onClick={handleSendMessage}
+                        className="bg-indigo-600 text-white p-2 rounded-r-md"
+                    >
+                        Send
+                    </button>
+                </div>
+            </div>
+
+            <div className="language-dropdown text-center">
+                <label htmlFor="language" className="block font-semibold">Select Language:</label>
+                <select
+                    id="language"
+                    value={selectedLanguage}
+                    onChange={handleLanguageChange}
+                    className="block w-full p-2 border border-gray-300 rounded-md"
+                >
+                    <option value="">Choose a language</option>
+                    <option value="language1">Language 1</option>
+                    <option value="language2">Language 2</option>
+                    {/* Add more language options as needed */}
+                </select>
+            </div>
+        </div>
+    );
 };
+
 
 export default ChatRoomPage;
